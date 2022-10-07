df6264970b7512360126ce85934f4336552b46ec
joosjegoedhart <joosje.goedhart@ibm.com>
2022-10-07 15:30:20 +0200

    initial comit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. I was changed. Am I still changed? Yes. 
         </p>
         <a
           className="App-link"
